2b02fd8cb0fdc15a5f0c082bb0f0d6af73295e02
michaelangeloojeani <128701443+michaelangeloojeani@users.noreply.github.com>
2025-04-24 23:49:44 +0100

    added dashboard function to get current user

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAuth } from '../contexts/AuthContext';
+import InstagramStats from './InstagramStats';
+import YouTubeStats from './YouTubeStats';
+
+function Dashboard() {
+  const { currentUser, logout } = useAuth(); // Get current user and logout method from auth context
+
+  // States for platform selection and username input
+  const [platform, setPlatform] = useState('instagram');
+  const [username, setUsername] = useState('');
+  const [searchUsername, setSearchUsername] = useState('');
